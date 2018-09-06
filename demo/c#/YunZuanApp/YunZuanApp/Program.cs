172259a7a0c7cce3579470210d8a6652f8d05970
ImbaQ <1299071860@qq.com>
2018-09-06 20:07:49 +0800

    add c# demo

---
+++ demo/c#/YunZuanApp/YunZuanApp/Program.cs
@@ -0,0 +1,139 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace YunZuanApp
+{
+    class Program
+    {
+        static string appKey = "appKey";
+        static string secretKey = "secretKey";
+        static string apiUrl = "https://yunzuan-api.bihe.one/";
+        //static string apiUrl = "http://def2bbe9.ngrok.io";
+        static void Main(string[] args)
+        {
+            appKey = "ZWYzZmZkZmVk";
+            secretKey = "zY0MzNmYzMxNzVhN2I3MmQwMDFkOTJjZTA4Z";
+            //appKey = "OTdhN2JmMGYw";
+            //secretKey = "Dk0ZWI3NzE4OGMzMWJjZjMwZDE1MmJjOGRkM";
+            Console.WriteLine(GetBalance("0x0A0d28c5D47E445D11B67aF7c2877e9220aF45cA"));
+            Console.WriteLine(GetTransactionCount("0x0A0d28c5D47E445D11B67aF7c2877e9220aF45cA"));
+            Console.WriteLine(GetTransactionList("0x0A0d28c5D47E445D11B67aF7c2877e9220aF45cA","",20));
+
+            Console.ReadKey();
+        }
+
+        static RestClient client = new RestClient()
+        {
+            BaseUrl = new Uri(apiUrl)
+        };
+        
+        /// <summary>
+        /// 余额
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public static string GetBalance(string address)
+        {
+            var request = new RestRequest("yz/getBalance", Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            //request.AddHeader("Host", "yunzuan-api.bihe.one");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("address", address);
+            var ts = Utils.GetTimeStamp();
+            param.Add("timestamp", ts);
+            var sign = GenSign(param);
+            Debug.WriteLine(sign);
+            request.AddHeader("sign", sign);
+            request.AddParameter("application/x-www-form-urlencoded", $"appkey={appKey}&address={address}&timestamp={ts}", ParameterType.RequestBody);
+            return client.Execute(request).Content;
+        }
+
+        /// <summary>
+        /// 交易次数
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public static string GetTransactionCount(string address)
+        {
+            var request = new RestRequest("yz/getTransactionCount",Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Host", "yunzuan-api.bihe.one");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("address", address);
+            var ts = Utils.GetTimeStamp();
+            param.Add("timestamp", ts);
+            var sign = GenSign(param);
+            request.AddHeader("sign", sign);
+            request.AddParameter("application/x-www-form-urlencoded", $"appkey={appKey}&address={address}&timestamp={ts}", ParameterType.RequestBody);
+            return client.Execute(request).Content;
+        }
+
+        /// <summary>
+        /// 交易记录
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="id"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string GetTransactionList(string address,string id,int size)
+        {
+            var request = new RestRequest("yz/getTransactionList",Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Host", "yunzuan-api.bihe.one");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("address", address);
+            param.Add("size", size.ToString());
+            param.Add("id", id);
+            var ts = Utils.GetTimeStamp();
+            param.Add("timestamp", ts);
+            var sign = GenSign(param);
+            request.AddHeader("sign", sign);
+            request.AddParameter("application/x-www-form-urlencoded", $"appkey={appKey}&address={address}&id={id}&size={size.ToString()}&timestamp={ts}", ParameterType.RequestBody);
+            return client.Execute(request).Content;
+        }
+
+        /// <summary>
+        /// 发送交易
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static string SendRawTransaction(string from,string to ,decimal amount)
+        {
+            var request = new RestRequest("yz/sendRawTransaction",Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Host", "yunzuan-api.bihe.one");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            var signTransation = Utils.SignTransation(from, to, amount);
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("tx", signTransation);
+            var ts = Utils.GetTimeStamp();
+            param.Add("timestamp", ts);
+            var sign = GenSign(param);
+            request.AddHeader("sign", sign);
+            request.AddParameter("application/x-www-form-urlencoded", $"appkey={appKey}&tx={signTransation}&timestamp={ts}", ParameterType.RequestBody);
+            return client.Execute(request).Content;
+        }
+
+        private static string GenSign(Dictionary<string, string> param)
+        {
+            param.Add("appkey", appKey);
+            param.Add("secretkey", secretKey);
+            SortedDictionary<string, string> sortedParam = new SortedDictionary<string, string>(param);
+            string str = "";
+            foreach (var kv in sortedParam)
+            {
+                str += kv.Key;
+                str += kv.Value;
+            }
+            Debug.WriteLine(str);
+            return Utils.GetSHA256(str);
+        }
+    }
+}
---
+++ demo/c#/YunZuanApp/YunZuanApp/Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YunZuanApp
+{
+    class Utils
+    {
+        public static string GetSHA256(string str)
+        {
+            byte[] bytValue = Encoding.UTF8.GetBytes(str);
+            try
+            {
+                SHA256 sha256 = new SHA256CryptoServiceProvider();
+                byte[] retVal = sha256.ComputeHash(bytValue);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                Debug.WriteLine("gen:"+sb.ToString());
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 签名交易
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static string SignTransation(string from, string to, decimal amount)
+        {
+            var privateKey = "privateKey";//转出钱包的私钥
+            var sign = new Nethereum.Signer.TransactionSigner();
+            var weiAmount = Nethereum.Util.UnitConversion.Convert.ToWei(amount);
+            BigInteger nonce_bigint = Convert.ToInt32(Program.GetTransactionCount(from), 16);
+            var gasLimit = new BigInteger(21000);
+            var gasPrice = new BigInteger(200000000000);
+            var txSign = sign.SignTransaction(privateKey, to, weiAmount, nonce_bigint, gasPrice, gasLimit);
+            return "0x" + txSign;
+        }
+
+        /// <summary>  
+        /// 获取当前时间戳  
+        /// </summary>  
+        /// <param name="bflag">为真时获取10位时间戳,为假时获取13位时间戳,默认bool bflag = false</param>  
+        /// <returns></returns>  
+        public static string GetTimeStamp(bool bflag = false)
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            string ret = string.Empty;
+            if (bflag)
+                ret = Convert.ToInt64(ts.TotalSeconds).ToString();
+            else
+                ret = Convert.ToInt64(ts.TotalMilliseconds).ToString();
+
+            return ret;
+        }
+    }
+}
